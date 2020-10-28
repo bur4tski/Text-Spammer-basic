c35901cc6ee3401e8de770e872cc62a5f2b566a2
hahadiko_alambatnandito <52223746+bur4tski@users.noreply.github.com>
2020-10-28 13:41:08 +0800

    textspammer v. 0.0.1
    Still beta, EXIT function are not applicable 
    Used for test only.

---
+++ text spammer.py
@@ -0,0 +1,13 @@
+rp = 'y'
+while rp == 'y' or rp == 'yes':
+	print('======TEXT DESTROYER|SPAMMER =======')
+	print('§§§§§§§§created by master burat§§§')
+	msg = input('please input your message: ')
+	print('||||||||⚠️warning limit is 400⚠️|||||||')
+	limit = int(input('please input your number of text: '))
+	if limit >= 400:
+		print('|||exceed limit|||')
+	else:
+		print('=========================')
+		for i in range(limit):
+			print(msg)
